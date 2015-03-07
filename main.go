6174000a93ef80a0f71938d8347d80546c82cea6
Adam Reeve <adreeve@gmail.com>
2015-03-07 21:11:15 +1300

    Separate reading and writing on server side
    Also implement ping/pong according to Gorilla docs.

--- main.go
+++ main.go
@@ -9,10 +9,22 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"code.google.com/p/go-uuid/uuid"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
+)
+
+const (
+	// Time allowed to write updates to the client.
+	writeWait = 10 * time.Second
+	// Time allowed to read the next pong message from the client.
+	pongWait = 60 * time.Second
+	// Send pings to client with this period. Must be less than pongWait.
+	pingPeriod = (pongWait * 9) / 10
+	// Send updates every:
+	updateInterval = 5 * time.Second
 )
 
 type Comment struct {
@@ -41,16 +53,58 @@ func getComments(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Error upgrading to websocket connection: %s", err)
 		return
 	}
+	go sendUpdates(conn)
+	readComments(conn)
+}
+
+func sendUpdates(conn *websocket.Conn) {
+	pingTicker := time.NewTicker(pingPeriod)
+	sendUpdatesTicker := time.NewTicker(updateInterval)
+	defer func() {
+		pingTicker.Stop()
+		sendUpdatesTicker.Stop()
+		conn.Close()
+	}()
+	// Send update immediately with initial data
+	if err := sendUpdate(conn); err != nil {
+		return
+	}
 	for {
-		messageType := websocket.TextMessage
-		p := loadJsonComments()
-
-		if err = conn.WriteMessage(messageType, p); err != nil {
-			return
+		select {
+		case <-pingTicker.C:
+			conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := conn.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
+				return
+			}
+		case <-sendUpdatesTicker.C:
+			if err := sendUpdate(conn); err != nil {
+				return
+			}
 		}
+	}
+}
 
-		messageType, r, err := conn.NextReader()
+func sendUpdate(conn *websocket.Conn) error {
+	messageType := websocket.TextMessage
+	p := loadJsonComments()
+
+	if err := conn.WriteMessage(messageType, p); err != nil {
+		return err
+	}
+	return nil
+}
+
+func readComments(conn *websocket.Conn) {
+	defer conn.Close()
+	conn.SetReadDeadline(time.Now().Add(pongWait))
+	conn.SetPongHandler(func(string) error {
+		conn.SetReadDeadline(time.Now().Add(pongWait))
+		return nil
+	})
+	for {
+		_, r, err := conn.NextReader()
 		if err != nil {
+			log.Printf("Error reading socket: %v", err)
 			return
 		} else {
 			newComment := decodeComment(r)
