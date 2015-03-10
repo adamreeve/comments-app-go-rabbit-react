8b70875abfe6baec59a171540d8915d1b0c5c906
Adam Reeve <adreeve@gmail.com>
2015-03-10 19:26:35 +1300

    Add example fanout arrangement with rabbit mq

---
+++ message-queue/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+const (
+	uri          = "amqp://guest:guest@172.17.0.7:5672/"
+	exchangeName = "test-exchange-fanout"
+	exchangeType = "fanout"
+	bindingKey   = "test-key"
+
+	reliable = true
+)
+
+func main() {
+	go receive("consumer1", "consumer1")
+
+	go receive("consumer2", "consumer2")
+
+	go publish()
+
+	log.Printf("Running forever")
+	select {}
+}
+
+func receive(queueName, consumerTag string) {
+	_, err := newConsumer(
+		uri,
+		exchangeName,
+		exchangeType,
+		queueName,
+		bindingKey,
+		consumerTag,
+	)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func publish() {
+	publisher, err := newPublisher(
+		uri,
+		exchangeName,
+		exchangeType,
+		reliable,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < 10; i++ {
+		message := fmt.Sprintf("Hello with message %d", i)
+		publisher.Publish(bindingKey, []byte(message))
+		time.Sleep(time.Second)
+	}
+}
